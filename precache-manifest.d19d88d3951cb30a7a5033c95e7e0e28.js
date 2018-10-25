2f5733a007de12dbf9182e1c4a1c151fa4a3f25d
sreepurnajasti <sreepurna.jasti@gmail.com>
2018-10-25 17:51:26 +0530

    Updates

---
+++ precache-manifest.d19d88d3951cb30a7a5033c95e7e0e28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2573dc96342ae71883a2",
+    "url": "/Poster/static/js/runtime~main.2573dc96.js"
+  },
+  {
+    "revision": "94a42f8fb0aba14df020",
+    "url": "/Poster/static/js/main.94a42f8f.chunk.js"
+  },
+  {
+    "revision": "0be802156d198890a615",
+    "url": "/Poster/static/js/1.0be80215.chunk.js"
+  },
+  {
+    "revision": "94a42f8fb0aba14df020",
+    "url": "/Poster/static/css/main.0878d179.chunk.css"
+  },
+  {
+    "revision": "7580589f5cf1c50ff156a73e17274ba5",
+    "url": "/Poster/index.html"
+  }
+];
